8eddd655e1bcb6c1bca3be5f5cb826f88746e4e3
Pavel Nikolaev <pnikolaev859@gmail.com>
2020-01-16 19:48:00 +0300

    add keydown event n' preventDefault

---
+++ js/script.js
@@ -0,0 +1,55 @@
+'use strict'
+
+const search = document.querySelector('.search');
+const cartBtn = document.querySelector('#cart');
+const cart = document.querySelector('.cart');
+const wishlistBtn = document.querySelector('#wishlist');
+
+const goodsWrapper = document.querySelector('.goods-wrapper');
+
+const createCartGoods = (id, title, price, img) => {
+  const card = document.createElement('div');
+  card.className = 'card-wrapper col-12 col-md-6 col-lg-4 col-xl-3 pb-3';
+  card.innerHTML = `
+    <div class="card">
+      <div class="card-img-wrapper">
+        <img class="card-img-top" src="${img}" alt="${title}">
+        <button class="card-add-wishlist" data-goods-id="${id}"></button>
+      </div>
+      <div class="card-body justify-content-between">
+        <a href="#" class="card-title">${title}</a>
+        <div class="card-price">${price}</div>
+        <div>
+          <button class="card-add-cart" data-goods-id="${id}">Добавить в корзину</button>
+        </div>
+      </div>
+    </div>
+  `;
+  return card;
+};
+
+goodsWrapper.append(createCartGoods(1, 'Дартс', 3033,'img/temp/archer.jpg'));
+goodsWrapper.append(createCartGoods(2, 'Фламинго', 2000, 'img/temp/flamingo.jpg'));
+goodsWrapper.append(createCartGoods(3, 'Носки', 230, 'img/temp/socks.jpg'));
+
+const openCart = () => {
+  cart.style.display = 'flex';
+};
+
+const closeCart = (event) => {
+  event.preventDefault();
+  const target = event.target;
+  console.log(target);
+  
+  if (target === cart || target.classList.contains('cart-close')) {
+    cart.style.display = '';
+  }
+};
+
+cartBtn.addEventListener('click', openCart);
+cart.addEventListener('click', closeCart);
+document.addEventListener("keydown", (event) => {
+  if (event.keyCode === 27) {
+    cart.style.display = '';
+  }
+});
